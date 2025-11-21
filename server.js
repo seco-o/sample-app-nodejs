a457ffa174703e62d382842af1a3887bfcadb478
seco-o <73754456+seco-o@users.noreply.github.com>
2025-11-21 10:14:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Azure Web App + GitHub Actions'dan merhaba!");
+  res.send("Devrim!");
 });
 
 app.listen(port, () => {
